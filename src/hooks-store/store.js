9d032458954c4762a4994309affc9151773fd091
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-24 21:22:52 +0300

    The implementation of the custom useStore hook was finalized

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
 
-const globalState = {};
+let globalState = {};
 let listeners = [];
+let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
+
+  const dispatch = actionId => {
+    const newState = actions[actionId](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
   useEffect(() => {
     listeners.push(setState);
@@ -13,4 +23,15 @@ const useStore = () => {
       listeners = listeners.filter(listener => listener !== setState);
     };
   }, [setState]);
+
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+
+  actions = { ...actions, ...userActions };
 };
